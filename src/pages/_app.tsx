a52c93a85edb8e4cbf17195326ae3865feade433
aldenfachetti <aldenmerlin@gmail.com>
2023-02-24 18:16:57 +0100

    Jest, React Testing Library and Styled Components included and set up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,23 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from '../styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>NextJS - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="description"
+          content="A simple project starter to work with ReactJS, TypeScript, NextJS and Styled-Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
